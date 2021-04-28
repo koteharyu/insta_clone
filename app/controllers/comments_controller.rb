43414edd9f01a64d6e8cfe6f8e357d539e471ee4
haryu <hryau6@gmail.com>
2021-04-28 20:16:20 +0900

    add: controller and views

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,31 @@
 class CommentsController < ApplicationController
+  before_action :require_login, only: %i[create edit update destroy]
+  
+  def create
+    @comment = current_user.comments.build(comment_params)
+    @comment.save
+  end
+
+  def edit
+    @comment = current_user.commets.find(params[:id])
+  end
+
+  def update
+    @comment = current_user.commets.find(params[:id])
+    @comment.update(comment_update_params)
+  end
+
+  def destroy
+    @comment = current_user.commets.find(params[:id])
+    @comment.destroy!
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:body).merge(post_id: params[:post_id])
+    end
+
+    def comemnt
+      params.require(:comment).permit(:body)
+    end
 end
