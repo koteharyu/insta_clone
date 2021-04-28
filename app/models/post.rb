b6cf056659c437470615248bd1c8f4ff9f85e06f
haryu <hryau6@gmail.com>
2021-04-28 20:21:50 +0900

    edit: locales ja

--- app/models/post.rb
+++ app/models/post.rb
@@ -21,7 +21,8 @@ class Post < ApplicationRecord
   mount_uploaders :images, PostImageUploader
   serialize :images, JSON
   belongs_to :user
-  
+  has_many :posts, dependent: :destroy
+
   validates :body, presence: true, length: { maximum: 1000}
   validates :images, presence: true
 end
