a527b5fc7023b6f548b6e9c9fb3ebb656f246630
haryu <hryau6@gmail.com>
2021-05-01 11:53:00 +0900

    ユニーク制約の追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_01_021507) do
+ActiveRecord::Schema.define(version: 2021_05_01_022105) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "body", null: false
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2021_05_01_021507) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id", "post_id"], name: "index_likes_on_user_id_and_post_id", unique: true
     t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
