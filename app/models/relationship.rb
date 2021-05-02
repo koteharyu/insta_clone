86f0aa7a300fa92b4ad4c9d2c98987984ecb5527
haryu <hryau6@gmail.com>
2021-05-02 15:37:30 +0900

    rails db:migrate and add not null to follower/followed_id

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,2 +1,18 @@
+# == Schema Information
+#
+# Table name: relationships
+#
+#  id          :bigint           not null, primary key
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  followed_id :integer          not null
+#  follower_id :integer          not null
+#
+# Indexes
+#
+#  index_relationships_on_followed_id                  (followed_id)
+#  index_relationships_on_followed_id_and_follower_id  (followed_id,follower_id) UNIQUE
+#  index_relationships_on_follower_id                  (follower_id)
+#
 class Relationship < ApplicationRecord
 end
