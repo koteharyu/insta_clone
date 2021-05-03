7d67272a79484d8a1a107961b66a9503dc3e7813
haryu <hryau6@gmail.com>
2021-05-03 11:56:45 +0900

    add: basic users_show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,10 @@ class UsersController < ApplicationController
     @users = User.all.page(params[:page]).order(created_at: :desc)
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
