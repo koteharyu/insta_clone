bde89d9c0c64b2bc6c0b4a82c64b80a74bbd93e2
haryu <hryau6@gmail.com>
2021-05-03 12:41:20 +0900

    add ajax with follow and unfollow

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,10 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
-  end
-
   def index
     @users = User.all.page(params[:page]).order(created_at: :desc)
   end
 
-  def show
-    @user = User.find(params[:id])
+  def new
+    @user = User.new
   end
 
   def create
@@ -22,9 +18,13 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:email, :password, :password_confirmation, :name)
   end
 end
