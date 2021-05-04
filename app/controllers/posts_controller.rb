1908f53fc57ee8375bca79881dfc0dd00dd7b165
haryu <hryau6@gmail.com>
2021-05-04 10:18:01 +0900

    add: search action to posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -61,6 +61,11 @@ class PostsController < ApplicationController
     render :index
   end
 
+  def search
+    @search_form = SearchPostsForm.new(params.require(:search).permit(:body))
+    @posts = @search_form.search.includes(:user).page(params[:page])
+  end
+
   private
     def post_params
       params.require(:post).permit(:body, images: [])
